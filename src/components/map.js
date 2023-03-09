91d3574d6cbfdd4ecd52d4c36171970574044d85
ciberdown <teymuri.amin@gmail.com>
2023-03-09 16:12:07 +0330

    first visual map

---
+++ src/components/map.js
@@ -0,0 +1,55 @@
+class Map {
+  constructor() {
+    this.size = 21;
+    this.canvas = document.getElementById("canvas").getContext("2d");
+    this.margin_left = 50;
+    this.margin_top = 20;
+    this.padding = 1;
+    this.encoder = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    this.create_map();
+  }
+  create_map() {
+    for (let i = 0; i < this.encoder.length; i++) {
+      for (let j = 0; j < this.encoder[0].length; j++) {
+        this.create_canvas(i, j, "red");
+      }
+    }
+    this.clear_canvas(1, 2);// 
+    this.create_canvas(1, 1, "green");//
+  }
+
+  create_canvas(x, y, color) {
+    this.canvas.fillStyle = color;
+    this.canvas.fillRect(
+      x * this.size + this.margin_left,
+      y * this.size + this.margin_top,
+      this.size - this.padding,
+      this.size - this.padding
+    );
+  }
+  clear_canvas(x, y) {
+    this.canvas.clearRect(
+      x * this.size + this.margin_left,
+      y * this.size + this.margin_top,
+      this.size - this.padding,
+      this.size - this.padding
+    );
+  }
+}
+
+const map = new Map();
